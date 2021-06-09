1c4c2e4fc7db4edfd1c253b9799d1a35131b4b80
fll02020 <77189099+fll02020@users.noreply.github.com>
2021-06-09 22:19:57 +0800

    Update simple_matrix.h

--- src/simple_matrix.h
+++ src/simple_matrix.h
@@ -1,28 +1,56 @@
+/*
+    MIT License
+    Copyright (c) 2021 ETCHKILI
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
 
+/**
+ * @Github https://github.com/ETCHKILI/cs205-project-simple-matrix
+ * @Organization SUSTech
+ * @Author Guo Yubin, Shen Zhilong, Fan Leilong
+ */
 #ifndef CS205_PROJECT_SIMPLE_MATRIX_SIMPLE_MATRIX_H
 #define CS205_PROJECT_SIMPLE_MATRIX_SIMPLE_MATRIX_H
 
 #include <cstdint>
-#include "simple_matrix_exception.h"
 #include <iterator>
+#include <vector>
+using namespace std;
 
-namespace simple_matrix {
+namespace simple_matrix
+{
     static const constexpr int64_t kDefaultRowSize = 10;
     static const constexpr int64_t kDefaultColumnSize = 10;
     static const constexpr int64_t kDefaultSideLength = 10;
     static const int64_t kMaxAllocateSize = UINT64_MAX;
+    static const double eps = 1e-3;
 
-    /*!
+    /**
      *
      * @class Matrix
      * @brief Base class in simple_matrix
      * @tparam T
      * @details Use one-dimension array to simulate a matrix
      */
-    template<typename T>
-    class Matrix {
+    template <typename T>
+    class Matrix
+    {
     private:
-        T* operator[](int row);
+        T *operator[](int row);
 
     protected:
         T *data_;
@@ -41,9 +69,17 @@ namespace simple_matrix {
         uint64_t getRowSize() const;
         uint64_t getColumnSize() const;
         static bool CheckSizeValid(uint64_t row_size, uint64_t column_size);
+        Matrix<T> Matrix<T>::identity(uint64_t s, T t);
+        Matrix<double> Householder(uint64_t col, uint64_t ele) const;
+        Matrix<double> Matrix<T>::Hessenberg() const;
+        Matrix<double> Matrix<T>::Givens(uint64_t col, uint64_t begin, uint64_t end) const;
+        Matrix<double> Matrix<T>::QR_iteration() const;
+        vector<double> Matrix<T>::eigenvalue() const;
+        Matrix<double> Matrix<T>::eigenvector() const;
+        inline Matrix<double> Matrix<T>::Nullspace() const;
     };
 
-    /*!
+    /**
      * @brief
      * @details
      * @tparam T
@@ -51,23 +87,26 @@ namespace simple_matrix {
      * @attention
      * @warning IT IS PROTECTED!!! Should only be used when you want to initialize the data_ as nullptr
      */
-    template<typename T>
-    Matrix<T>::Matrix() {
+    template <typename T>
+    Matrix<T>::Matrix()
+    {
         data_ = nullptr;
-        row_size_ = kDefaultRowSize;
-        column_size_ = kDefaultColumnSize;
+        row_size_ = 0;
+        column_size_ = 0;
     }
 
-    /*!
+    /**
      * Constructor that do not initialize the data
      *
      * @tparam T
      * @param row_size
      * @param column_size
      */
-    template<typename T>
-    Matrix<T>::Matrix(uint64_t row_size, uint64_t column_size) {
-        if ( !CheckSizeValid(row_size, column_size)) {
+    template <typename T>
+    Matrix<T>::Matrix(uint64_t row_size, uint64_t column_size)
+    {
+        if (!CheckSizeValid(row_size, column_size))
+        {
             throw simple_matrix::BadSizeException("Size too large!");
         }
         data_ = new T[row_size * column_size];
@@ -83,130 +122,271 @@ namespace simple_matrix {
      * @param column_size
      * @param initial_value
      */
-    template<typename T>
-    Matrix<T>::Matrix(uint64_t row_size, uint64_t column_size, T initial_value) {
-        if ( !CheckSizeValid(row_size, column_size)) {
+    template <typename T>
+    Matrix<T>::Matrix(uint64_t row_size, uint64_t column_size, T initial_value)
+    {
+        if (!CheckSizeValid(row_size, column_size))
+        {
             throw simple_matrix::BadSizeException("Size too large!");
         }
         data_ = new T[row_size * column_size];
         column_size_ = column_size;
         row_size_ = row_size;
-        for (int i = 0; i < row_size_; ++i) {
-            for (int j = 0; j < column_size_; ++j) {
+        for (int i = 0; i < row_size_; ++i)
+        {
+            for (int j = 0; j < column_size_; ++j)
+            {
                 data_[i * column_size_ + j] = initial_value;
             }
         }
     }
 
-    /*!
+    /**
     *
     * @tparam T
     * @param row_size
     * @param column_size
     * @param initial_value
     */
-    template<typename T>
-    Matrix<T>::~Matrix() {
-        if (data_ != nullptr) {
-            delete []data_;
+    template <typename T>
+    Matrix<T>::~Matrix()
+    {
+        if (data_ != nullptr)
+        {
+            delete[] data_;
         }
     }
 
-    /*!
-     * @deprecated
-     * @warning THIS IS PROTECTED. Use Access(int, int) instead
+    /**
+     * @protected by simple_matrix::Matrix
+     * @warning THIS IS PROTECTED. Use Access(int, int) to access the
+     * data unless you are writing a derived class and want to access
+     * the data directly.
      * @tparam T
      * @param row
      * @return
      */
-    template<typename T>
-    T *Matrix<T>::operator[](int row) {
+    template <typename T>
+    T *Matrix<T>::operator[](int row)
+    {
         return data_ + row * column_size_;
     }
 
-    /*!
+    /**
      *
      * @tparam T
      * @param row_size
      * @param column_size
      * @return
      */
-    template<typename T>
-    bool Matrix<T>::CheckSizeValid(uint64_t row_size, uint64_t column_size) {
-        return (row_size <= kMaxAllocateSize / column_size) || (column_size <= kMaxAllocateSize / row_size);
+    template <typename T>
+    bool Matrix<T>::CheckSizeValid(uint64_t row_size, uint64_t column_size)
+    {
+        return ((row_size <= kMaxAllocateSize / column_size) || (column_size <= kMaxAllocateSize / row_size)) && std::max(row_size, column_size) > 0;
     }
 
-    /*!
+    /**
      *
      * @tparam T
      * @param val
      */
-    template<typename T>
-    void Matrix<T>::SetValue(T val) {
+    template <typename T>
+    void Matrix<T>::SetValue(T val)
+    {
         /// TODO handle this exception
-        if (data_ == nullptr) {
-            return;
+        if (data_ == nullptr)
+        {
+            throw BadAccessException("Matrix is not initialized or index out of range");
         }
-        for (int i = 0; i < row_size_; ++i) {
-            for (int j = 0; j < column_size_; ++j) {
+        for (int i = 0; i < row_size_; ++i)
+        {
+            for (int j = 0; j < column_size_; ++j)
+            {
                 data_[i * column_size_ + j] = val;
             }
         }
     }
 
-    /*!
+    /**
      *
      * @tparam T
      * @param row
      * @param column
      * @return
      */
-    template<typename T>
-    T &Matrix<T>::Access(uint64_t row, uint64_t column) {
+    template <typename T>
+    T &Matrix<T>::Access(uint64_t row, uint64_t column)
+    {
+        if (row > row_size_ - 1 || column > column_size_ - 1)
+        {
+            throw BadAccessException("Index out of bounds");
+        }
         return (*this)[row][column];
     }
 
-    /*!
+    /**
      *
      * @tparam T
      * @return
      */
-    template<typename T>
-    uint64_t Matrix<T>::getRowSize() const {
+    template <typename T>
+    uint64_t Matrix<T>::getRowSize() const
+    {
         return row_size_;
     }
 
-    /*!
+    /**
      *
      * @tparam T
      * @return
      */
-    template<typename T>
-    uint64_t Matrix<T>::getColumnSize() const {
+    template <typename T>
+    uint64_t Matrix<T>::getColumnSize() const
+    {
         return column_size_;
     }
 
-    /*!
+    /**
+     * @protected by Matrix, you should only use this when
+     * you want to initialize the data of matrix IN YOUR DERIVED
+     * CLASS and have to set size for initializing matrix
      *
      * @tparam T
      * @param rowSize
      */
-    template<typename T>
-    void Matrix<T>::setRowSize(uint64_t rowSize) {
+    template <typename T>
+    void Matrix<T>::setRowSize(uint64_t rowSize)
+    {
         row_size_ = rowSize;
     }
 
-    /*!
+    /**
+     * @protected by Matrix, you should only use this when
+     * you want to initialize the data of matrix IN YOUR DERIVED
+     * CLASS and have to set size for initializing matrix
      *
      * @tparam T
      * @param columnSize
      */
-    template<typename T>
-    void Matrix<T>::setColumnSize(uint64_t columnSize) {
+    template <typename T>
+    void Matrix<T>::setColumnSize(uint64_t columnSize)
+    {
         column_size_ = columnSize;
     }
 
+    template <typename T>
+    Matrix<T> Matrix<T>::identity(uint64_t s, T t)
+    {
+        Matrix<T> res(s, s);
+        for (uint64_t i = 0; i < s; ++i)
+        {
+            res.Access(i, i) = t;
+        }
+        return res;
+    }
 
+    template <typename T>
+    Matrix<double> Matrix<T>::Householder(uint64_t col, uint64_t ele) const
+    {
+        double square = 0;
+        for (uint64_t i = ele - 1; i < row_size_ * column_size_; ++i)
+        {
+            square += pow(this->Access(i, col - 1), 2);
+        }
+        double mod = this->Access(ele - 1, col - 1) > 0 ? pow(square, 0.5) : -pow(square, 0.5);
+        double modulus = mod * (mod + this->Access(ele - 1, col - 1));
+        Matrix<double> U;
+        U.SetValue(1);
+        for (int j = 0; j < this->row_size_; ++j)
+        {
+            if (j > ele - 1)
+            {
+                U.Access(j, 0) = this->Access(j, col - 1);
+            }
+            else
+            {
+                U.Access(j, 0) = 0;
+            }
+        }
+        U.Access(ele - 1, 0) = this->Access(ele - 1, col - 1) + mod;
+        return Matrix<double>::identity(this->row_size_, 1) - U * U.transpose() / modulus;
+    }
+
+    template <typename T>
+    Matrix<double> Matrix<T>::Hessenberg() const
+    {
+        if (this->column_size_ != this->row_size_)
+        {
+            throw std::invalid_argument("The matrix needs to be square");
+        }
+        Matrix<double> left_H = Matrix<double>::identity(this->row_size_, 1);
+        Matrix<double> right_H = Matrix<double>::identity(this->row_size_, 1);
+        Matrix<double> H = left_H * this * right_H;
+        for (int i = 1; i < this->row_size_ - 1; ++i)
+        {
+            if (abs(H.Access(i + 1, i - 1)) > eps)
+            {
+                left_H = left_H * H.Householder(i, i + 1);
+                right_H = H.Householder(i, i + 1) * right_H;
+                H = left_H * this * right_H;
+            }
+        }
+        return H;
+    }
+
+    template <typename T>
+    Matrix<double> Matrix<T>::Givens(uint64_t col, uint64_t begin, uint64_t end) const
+    {
+        Matrix<double> R = Matrix<double>::identity(this->row_size_, 1);
+        double r = pow(pow(this->Access(begin - 1, col - 1), 2) + pow(this->Access(end - 1, col - 1), 2), 0.5);
+        double c = 1;
+        double s = 0;
+        if (abs(r) > eps)
+        {
+            c = this->Access(begin - 1, col - 1) / r;
+            s = this->Access(end - 1, col - 1) / r;
+        }
+        R.Access(begin - 1, begin - 1) = c;
+        R.Access(begin - 1, end - 1) = s;
+        R.Access(end - 1, begin - 1) = -s;
+        R.Access(end - 1, end - 1) = c;
+        return R;
+    }
+
+    template <typename T>
+    Matrix<double> Matrix<T>::QR_iteration() const
+    {
+        Matrix<double> R = this->Hessenberg();
+        Matrix<double> Q = Matrix<double>::identity((this->row_size_) * (this->column_size_), 1);
+        for (uint64_t i = 1; i < (this->row_size_) * (this->column_size_); ++i)
+        {
+            Matrix<double> temp_R = R.Givens(i, i, i + 1);
+            R = temp_R * R;
+            Q = Q * temp_R.transpose();
+        }
+        return R * Q;
+    }
+
+    template <typename T>
+    vector<double> Matrix<T>::eigenvalue() const
+    {
+        if (this->column_size_ != this->row_size_)
+        {
+            throw std::invalid_argument("The matrix needs to be square");
+        }
+        vector<double> eigenvalues(this->row_size_);
+        static constexpr int32_t iter_times = 150;
+        Matrix<double> H = this->QR_iteration();
+        for (int i = 0; i < iter_times; ++i)
+        {
+            H = H.QR_iteration();
+        }
+        for (int j = 0; j < this->row_size_; ++j)
+        {
+            eigenvalues[j] = H.Access(j, j);
+        }
+        return eigenvalues;
+    }
 
     /*!
      * TODO complete it if we still have time :)
@@ -216,10 +396,10 @@ namespace simple_matrix {
      * @details Use one-dimension array to simulate a matrix
      *          Considering it is symmetric, save data[i][j] in the same space as data[j][i]
      */
-    template<typename T>
-    class SymmetricMatrix : public Matrix<T>{
+    template <typename T>
+    class SymmetricMatrix : public Matrix<T>
+    {
     private:
-
     public:
         explicit SymmetricMatrix();
         explicit SymmetricMatrix(uint64_t side_length);
@@ -228,16 +408,19 @@ namespace simple_matrix {
         static bool CheckSizeValid(uint64_t side_length);
     };
 
-    template<typename T>
-    SymmetricMatrix<T>::SymmetricMatrix() {
+    template <typename T>
+    SymmetricMatrix<T>::SymmetricMatrix()
+    {
         this->data_ = new T[kDefaultSideLength * (kDefaultSideLength - 1) / 2];
         this->setColumnSize(kDefaultSideLength);
         this->setRowSize(kDefaultSideLength);
     }
 
-    template<typename T>
-    SymmetricMatrix<T>::SymmetricMatrix(uint64_t side_length) {
-        if (!CheckSizeValid(side_length)) {
+    template <typename T>
+    SymmetricMatrix<T>::SymmetricMatrix(uint64_t side_length)
+    {
+        if (!CheckSizeValid(side_length))
+        {
             throw simple_matrix::BadSizeException("Size too large");
         }
         this->data_ = new T[side_length * (side_length - 1) / 2];
@@ -245,23 +428,28 @@ namespace simple_matrix {
         this->setRowSize(side_length);
     }
 
-    template<typename T>
-    SymmetricMatrix<T>::SymmetricMatrix(uint64_t side_length, T initial_value) {
-        if (!CheckSizeValid(side_length)) {
+    template <typename T>
+    SymmetricMatrix<T>::SymmetricMatrix(uint64_t side_length, T initial_value)
+    {
+        if (!CheckSizeValid(side_length))
+        {
             throw simple_matrix::BadSizeException("Size too large");
         }
-        this->data_ = new T[side_length * (side_length - 1) / 2] {initial_value};
+        this->data_ = new T[side_length * (side_length - 1) / 2]{initial_value};
         this->setColumnSize(side_length);
         this->setRowSize(side_length);
+        Vector<T>(1, 1, 1);
     }
 
-    template<typename T>
-    bool SymmetricMatrix<T>::CheckSizeValid(uint64_t side_length) {
+    template <typename T>
+    bool SymmetricMatrix<T>::CheckSizeValid(uint64_t side_length)
+    {
         return side_length <= kMaxAllocateSize / side_length;
     }
 
-    template<typename T>
-    SymmetricMatrix<T>::~SymmetricMatrix() {
+    template <typename T>
+    SymmetricMatrix<T>::~SymmetricMatrix()
+    {
     }
 }
 
