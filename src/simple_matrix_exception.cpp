24e7104c636b8d29b01f9ad9307f9f8e8dd726d6
Etch <guoyubinetch@gmail.com>
2021-06-10 16:48:48 +0800

    #try2

--- src/simple_matrix_exception.cpp
+++ src/simple_matrix_exception.cpp
@@ -1,7 +1,3 @@
-//
-// Created by GuoYubin on 2021/6/8.
-//
-
 #include "simple_matrix_exception.h"
 
 simple_matrix::BadSizeException::BadSizeException(const std::string &arg) : length_error(arg) {}
