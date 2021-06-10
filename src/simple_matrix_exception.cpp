0ae92c46aaea15c63e0a759f188b6032a5e47f55
fll02020 <77189099+fll02020@users.noreply.github.com>
2021-06-10 13:24:55 +0800

    Update simple_matrix_exception.cpp

--- src/simple_matrix_exception.cpp
+++ src/simple_matrix_exception.cpp
@@ -1,7 +1,7 @@
-//
-// Created by GuoYubin on 2021/6/8.
-//
-
 #include "simple_matrix_exception.h"
 
 simple_matrix::BadSizeException::BadSizeException(const std::string &arg) : length_error(arg) {}
+
+simple_matrix::BadAccessException::BadAccessException(const std::string &arg) : out_of_range(arg) {}
+
+simple_matrix::ArgumentNotMatchException::ArgumentNotMatchException(const std::string &arg) : invalid_argument(arg) {}
